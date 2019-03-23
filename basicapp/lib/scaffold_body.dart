ce333ffc3f77306388cc379d592464c51a680d54
chandru1807 <chandru18071995@gmail.com>
2019-03-23 19:30:05 +0530

    added bottom navigation bar and other stylings

--- basicapp/lib/scaffold_body.dart
+++ basicapp/lib/scaffold_body.dart
@@ -6,11 +6,12 @@ class ScaffoldBody extends StatefulWidget {
 }
 
 class _ScaffoldBodyState extends State<ScaffoldBody> {
+  bool _isSelected = true;
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.only(left: 14.0),
+        padding: const EdgeInsets.only(left: 10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -34,27 +35,51 @@ class _ScaffoldBodyState extends State<ScaffoldBody> {
               ),
             ),
             SizedBox(
-              height: 20.0,
+              height: 15.0,
             ),
             cardContainer("assets/kal-visuals-644747-unsplash.jpg",
-                "Whiskey in a teacup"),
+                "Whiskey in a teacup", true),
             SizedBox(
-              height: 10.0,
+              height: 15.0,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                materialChoice("Featured"),
-                materialChoice("All Categories"),
+                Column(
+                  children: <Widget>[
+                    materialChoice("Featured"),
+                    SizedBox(height: 5.0),
+                    _isSelected == true
+                        ? Container(
+                            height: 4.0,
+                            width: "Featured".length * 10.0,
+                            color: Colors.deepOrange,
+                          )
+                        : Container()
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    materialChoice("All Categories"),
+                    SizedBox(height: 5.0),
+                    _isSelected == false
+                        ? Container(
+                            height: 4.0,
+                            width: "All Categories".length * 10.0,
+                            color: Colors.deepOrange,
+                          )
+                        : Container()
+                  ],
+                ),
               ],
             ),
             SizedBox(
-              height: 20.0,
+              height: 15.0,
             ),
             cardContainer("assets/nik-shuliahin-201330-unsplash.jpg",
-                "Crazy Rich Asians"),
+                "Crazy Rich Asians", false),
             SizedBox(
-              height: 20.0,
+              height: 15.0,
             ),
             Text(
               "Recommended for you",
@@ -65,23 +90,20 @@ class _ScaffoldBodyState extends State<ScaffoldBody> {
               ),
             ),
             SizedBox(
-              height: 20.0,
+              height: 15.0,
             ),
             cardContainer("assets/nik-shuliahin-201330-unsplash.jpg",
-                "Crazy Rich Asians"),
+                "Crazy Rich Asians", false),
           ],
         ),
       ),
     );
   }
-}
 
-Material materialChoice(String text) {
-  return Material(
-    //elevation: 5.0,
-    //shadowColor: Colors.blueAccent,
-    child: InkWell(
-      //highlightColor: Colors.red,
+  InkWell materialChoice(String text) {
+    return InkWell(
+      highlightColor: Colors.red,
+      enableFeedback: true,
       customBorder: Border.all(style: BorderStyle.solid, width: 40.0),
       child: Text(
         text,
@@ -90,26 +112,30 @@ Material materialChoice(String text) {
           fontWeight: FontWeight.bold,
         ),
       ),
-      //splashColor: Colors.amber,
-      onTap: () {},
-    ),
-  );
+      splashColor: Colors.amber,
+      onTap: () {
+        setState(() {
+          _isSelected = !_isSelected;
+        });
+      },
+    );
+  }
 }
 
-Container cardContainer(String imageUrl, String name) {
+Container cardContainer(String imageUrl, String name, bool progress) {
   return Container(
-    height: 220.0,
+    height: 173.0,
     child: ListView.builder(
       scrollDirection: Axis.horizontal,
       itemCount: 5,
       itemBuilder: (BuildContext context, int index) {
         return Padding(
-          padding: const EdgeInsets.only(right: 20.0),
+          padding: const EdgeInsets.only(right: 5.0),
           child: Column(
             children: <Widget>[
               Container(
-                height: 150.0,
-                width: 150.0,
+                height: 130.0,
+                width: 130.0,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(10.0)),
                   image: DecorationImage(
@@ -122,14 +148,16 @@ Container cardContainer(String imageUrl, String name) {
               Container(
                 height: 4.0,
                 width: 150.0,
-                margin: EdgeInsets.only(bottom: 10.0),
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                  colors: [
-                    Colors.yellow,
-                    Colors.black,
-                  ],
-                )),
+                margin: progress == true ? EdgeInsets.only(bottom: 10.0) : null,
+                decoration: progress == true
+                    ? BoxDecoration(
+                        gradient: LinearGradient(
+                        colors: [
+                          Colors.yellow,
+                          Colors.black,
+                        ],
+                      ))
+                    : null,
               ),
               Text(
                 name,
